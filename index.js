b0697096ce0530cb7479c0a9600b946784656418
edmccrea <ed-mccrea@hotmail.co.uk>
2022-03-14 17:12:50 +0100

    script update

--- index.js
+++ index.js
@@ -16,6 +16,17 @@ app.use('/users', require('./routes/users'));
 app.use('/auth', require('./routes/auth'));
 app.use('/week', require('./routes/week'));
 
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  // Step 1:
+  app.use(express.static(path.resolve(__dirname, './client/build')));
+  // Step 2:
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
