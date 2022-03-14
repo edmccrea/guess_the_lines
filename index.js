49ba23e0eeced7c0304794474194e4e0b920ce93
edmccrea <ed-mccrea@hotmail.co.uk>
2022-03-14 20:49:35 +0100

    heorku deploy test

--- index.js
+++ index.js
@@ -7,25 +7,25 @@ const app = express();
 
 connectDB();
 
-if (process.env.NODE_ENV === 'production') {
-  // Step 1:
-  app.use(express.static(path.resolve('client/build')));
-  // Step 2:
-  app.use('*', express.static(path.join(__dirname, 'client', 'build')));
-}
-
 app.use(express.json({ extended: false }));
 app.use(cors());
 app.use(bodyParser.json());
-app.get('/', (req, res) => res.send('API Running'));
+
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.use('/games', require('./routes/games'));
 app.use('/picks', require('./routes/picks'));
 app.use('/users', require('./routes/users'));
 app.use('/auth', require('./routes/auth'));
 app.use('/week', require('./routes/week'));
-
-const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
